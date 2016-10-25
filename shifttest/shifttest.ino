3809fc83d3b4a06d7c3f54ca2a071dfceca64982
Cau5tik <cau5tik@cubenet.ca>
2016-10-24 20:25:37 -0700

    the thing

--- shifttest/shifttest.ino
+++ shifttest/shifttest.ino
@@ -12,34 +12,38 @@ void setup() {
   pinMode(storClk,OUTPUT);
   pinMode(shiftClk,OUTPUT);
   pinMode(reset,OUTPUT);
+  Serial.begin(9600);
 
 }
-boolean registers[16];
+boolean registers[16]= {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1};
 
 
 void writeReg() {
 
-  digitalWrite(storClk,LOW);
-
-  for (int i=15;i>=0;i--){
+ digitalWrite(storClk,LOW);
+ for (int i=15;i>=0;i--){
     digitalWrite(shiftClk,LOW);
     digitalWrite(data,registers[i]);
+    delayMicroseconds(1000);
     digitalWrite(shiftClk,HIGH);
-  }
-  digitalWrite(storClk,HIGH);
-
+ }
+ digitalWrite(storClk,HIGH);
 }
 
 void loop(){
-  for (int i=0; i<16; i++){
-    registers[i]=HIGH;
-    delay(100);
+    
+  int j;
+  for (int i=0;i<16;i++){
+    registers[i]=1;
+    if (i > 1){
+      j=i-1;
+      registers[j]=0;
+    }
     writeReg();
+    if (i == 15){
+      registers[i]=0;
+    }
   }
-  for (int i=15; i>=0;i--){
-    registers[i]=LOW;
-    delay(100);
-    writeReg();
-  }
+
 }
 
